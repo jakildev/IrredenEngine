af8a7426b947591050b3b3577ec83cba6b96c16d
Evin Killian <jakildev@gmail.com>
2026-05-30 23:51:01 -0700

    Revert "render: compensate WASD camera movement for yaw rotation (#1352)"
    This reverts commit baebe23db3d1ede253a782c40d7096d3a27e916d.

--- engine/prefabs/irreden/render/systems/system_render_velocity_2d_iso.hpp
+++ engine/prefabs/irreden/render/systems/system_render_velocity_2d_iso.hpp
@@ -2,7 +2,6 @@
 #define SYSTEM_RENDER_VELOCITY_2D_ISO_H
 
 #include <irreden/ir_time.hpp>
-#include <irreden/render/camera.hpp>
 
 #include <irreden/render/components/component_camera.hpp>
 #include <irreden/render/components/component_zoom_level.hpp>
@@ -15,14 +14,9 @@ namespace IRSystem {
 
 template <> struct System<RENDERING_VELOCITY_2D_ISO> {
     static constexpr Concurrency kConcurrency = Concurrency::PARALLEL_FOR;
-    float cameraYaw_ = 0.0f;
-
-    void beginTick() {
-        cameraYaw_ = IRPrefab::Camera::getYaw();
-    }
 
     void tick(C_Position2DIso &position, const C_Velocity2DIso &velocity) {
-        position.pos_ += IRMath::rotate2D(velocity.velocity_, cameraYaw_) *
+        position.pos_ += velocity.velocity_ *
                          // TODO: Delta time based on event registered in pipeline
                          vec2(IRTime::deltaTime(IRTime::RENDER));
     }
